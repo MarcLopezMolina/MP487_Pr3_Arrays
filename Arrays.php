971b846d5bc1b1dff013f81169e72cb02235ea12
MarcLopezMolina <marclopezmolina242006@gmail.com>
2025-01-06 20:51:42 +0100

    Explanation of every line

--- Arrays.php
+++ Arrays.php
@@ -9,116 +9,160 @@
 <body>
     <?php
 
-        //Mensaje básico por consola
+        //EJERCICIO 1
         echo "<h1>Ejercicio 1</h1>";
 
-        $datos = array('nombre' => 'Sara', 'apellido' => 'Martínez', 'edad' => 23, 'ciudad' => 'Barcelona');
+        //Array asociativo donde asignamos a cada clave su valor correspondiente.
+        $datos = array('nombre' => 'Sara', 'apellido' => 'Martinez', 'edad' => 23, 'ciudad' => 'Barcelona');
 
-        $num = 1;
+        //Variable auxiliar para contar el número de dato.
+        $num1 = 1;
 
-        foreach ($datos as $value) 
+        //Estructura foreach donde mostramos cada número de dato seguido de su valor correspondiente. 
+        foreach ($datos as $valor) 
         {
-            echo "dato $num º: $value <br>";
-            $num++;
+            echo "dato " . $num1 . "º: " . $valor . "<br>";
+            $num1++;
         }
 
-        //
+
+
+        //EJERCICIO 2
         echo "<br><h1>Ejercicio 2</h1>";
 
-        foreach ($datos as $clave => $value) 
+        //Estructura foreach donde mostramos cada clave seguido de su valor correspondiente.
+        foreach ($datos as $clave => $valor) 
         {
-            echo "$clave: $value <br>";
+            echo $clave . ": " . $valor . "<br>";
         }
 
 
+
+        //EJERCICIO 3
         echo "<br><h1>Ejercicio 3</h1>";
 
+        //Actualizamos el valor de la clave edad a 24.
         $datos['edad'] = 24;
 
-        $num = 1;
+        //Variable auxiliar para contar el número de dato.
+        $num2 = 1;
 
-        foreach ($datos as $value) {
-            echo "dato $num º: $value <br>";
-            $num++;
+        //Estructura foreach donde mostramos cada número de dato seguido de su valor correspondiente. 
+        foreach ($datos as $valor) {
+            echo "dato " . $num2 . "º: " . $valor . "<br>";
+            $num2++;
         }
 
 
+
+        //EJERCICIO 4
         echo "<br><h1>Ejercicio 4</h1>";
 
+        //Borramos la clave de ciudad del array $datos.
         unset($datos['ciudad']);
 
+        //Mostramos su contenido utilizando la función var_dump.
         var_dump($datos);
 
 
+
+        //EJERCICIO 5
         echo "<br><br><h1>Ejercicio 5</h1>";
 
+        //Declaramos un nuevo array con letras del abecedario como valores.
         $letters = 'a,b,c,d,e,f';
 
-        $letras = explode(',', $letters);
+        //Separamos sus valores por coma usando la función explode.
+        $letters = explode(',', $letters);
 
-        arsort($letras);
+        //Ordenamos el array de forma inversa. 
+        arsort($letters);
 
-        $num = 6;
+        //Variable auxiliar para contar el número de dato de manera inversa.
+        $num3 = 6;
 
-        foreach ($letras as $abecedario) 
+        //Estructura foreach donde mostramos cada letra ordenada de forma inversa junto con el valor del array correspondiente también ordenado de forma inversa.
+        foreach ($letters as $valor) 
         {
-            echo "value $num º: $abecedario <br>";
-            $num--;
+            echo "letter " . $num3 . "º: " . $valor . "<br>";
+            $num3--;
         }
 
 
+
+        //EJERCICIO 6
         echo "<br><h1>Ejercicio 6</h1>";
 
+        //Array asociativo donde asignamos a cada clave su valor correspondiente.
         $notas = array('Miguel' => 5, 'Luís' => 7, 'Marta' => 10, 'Isabel' => 8, 'Aitor' => 4, 'Pepe' => 1);
 
+        //Ordenamos el array anterior de mayor a menor por el valor de cada clave.
         arsort($notas);
 
+        //Simple mensaje
         echo "Notas de los estudiantes: ";
-        foreach ($notas as $clave => $value) 
+
+        //Estructura foreach donde mostramos cada clave seguido de su valor correspondiente.
+        foreach ($notas as $clave => $valor) 
         {
-            echo "$clave: $value, ";
+            echo $clave . ": " . $valor . " ";
         }
+        
 
 
+        //EJERCICIO 7
         echo "<br><br><h1>Ejercicio 7</h1>";
 
-        $sumaNotas = 0;
+        //Dos simples variables para contar la suma de todas las notas y la cantidad total de alumnos.
+        $sumaTotal = 0;
+        $cantidadAlumnos = 0;
 
-        $cantidad = 0;
-
+        //Estructura foreach donde recorremos el array de $notas con el objetivo de conocer la suma de todas las notas y la cantidad total de alumnos.
         foreach ($notas as $nota) 
         {
-            $sumaNotas += $nota;
-            $cantidad++;
+            $sumaTotal += $nota;
+            $cantidadAlumnos++;
         }
 
-        $notaMedia = round(($sumaNotas / $cantidad), 2);
+        //Variable para calcular la media de todas las notas de los alumnos, con solo 2 decimales.
+        $notaMedia = round(($sumaTotal / $cantidadAlumnos), 2);
 
-        echo "Media de las notas: $notaMedia <br>";
+        //Simple mensaje para mostrar la media de las notas.
+        echo "Media de las notas: " . $notaMedia . "<br>";
+
+        //Simple mensaje
         echo "Alumnos con nota por encima de la media: <br>";
 
-
-        foreach ($notas as $clave => $value) 
+        //Estructura foreach donde mostramos las claves (nombres de los alumnos) cuyos valores superan la media de la clase.
+        foreach ($notas as $clave => $valor) 
         {
-            if ($value > $notaMedia) {
+            if ($valor > $notaMedia) 
+            {
                 echo "$clave <br>";
             }
         }
 
 
+
+        //EJERCICIO 8
         echo "<br><h1>Ejercicio 8</h1>";
 
-        $notaAlta = 0;
+        //Dos simples variables para almacenar los datos de cual es la nota más alta y el alumno que ha obtenido dicho resultado.
+        $notaMasAlta = 0;
         $alumno;
 
-        foreach ($notas as $clave => $value) 
+        //Estructura foreach donde recorremos el array $notas para ir comparando cada valor con el más grande obtenido préviamente, hasta dar con la nota más alta y su alumno en cuestión.
+        foreach ($notas as $clave => $valor) 
         {
-            if($value > $notaAlta){
-                $notaAlta = $value;
+            if($valor > $notaMasAlta)
+            {
+                $notaMasAlta = $valor;
                 $alumno = $clave;
             }
         }
-        echo "La nota mas alta es $notaAlta y el mejor alumno es $alumno. <br>";
+
+        //Simple mensaje informativo de cual es la nota más alta y su alumno en cuestión.
+        echo "La nota mas alta es " . $notaMasAlta . " y el mejor alumno es " . $alumno . "<br>";
     ?>
 
 </body>
--- MiTiendaComics.php
+++ MiTiendaComics.php
@@ -1,97 +1,93 @@
 <?php
-// Definición del inventario de cómics
-$inventario = [
-    'suspense_terror' => 
-    [
-        ['titulo' => 'The Long Halloween', 'editorial' => 'DC', 'autor' => 'Tim Sale', 'idioma' => 'Inglés', 'precio' => 20, 'stock' => 10],
-        ['titulo' => 'Uzumaki', 'editorial' => 'Planeta', 'autor' => 'Junji Ito', 'idioma' => 'Japonés', 'precio' => 25, 'stock' => 15],
-        ['titulo' => 'Tomie', 'editorial' => 'Planeta', 'autor' => 'Junji Ito', 'idioma' => 'Japonés', 'precio' => 25, 'stock' => 20],
-    ],
-    'accion' => 
-    [
-        ['titulo' => 'Berserk Deluxe Edition 1', 'editorial' => 'Dark Horse', 'autor' => 'Kentaro Miura', 'idioma' => 'Japonés', 'precio' => 30, 'stock' => 12],
-    ],
-    // Puedes agregar más categorías y cómics según sea necesario
-];
+    // Definición del inventario de cómics
+    $inventario = [
+        'suspense_terror' => 
+        [
+            ['titulo' => 'The Long Halloween', 'editorial' => 'DC', 'autor' => 'Tim Sale', 'idioma' => 'Inglés', 'precio' => 20, 'stock' => 10],
+            ['titulo' => 'Uzumaki', 'editorial' => 'Planeta', 'autor' => 'Junji Ito', 'idioma' => 'Japonés', 'precio' => 25, 'stock' => 15],
+            ['titulo' => 'Tomie', 'editorial' => 'Planeta', 'autor' => 'Junji Ito', 'idioma' => 'Japonés', 'precio' => 25, 'stock' => 20],
+        ],
+        'accion' => 
+        [
+            ['titulo' => 'Berserk Deluxe Edition 1', 'editorial' => 'Dark Horse', 'autor' => 'Kentaro Miura', 'idioma' => 'Japonés', 'precio' => 30, 'stock' => 12],
+        ],
+        // Puedes agregar más categorías y cómics según sea necesario
+    ];
 
-// main
-// antes descuento
-mostrarComicsEnTabla();
-mostrarValorAlmacen();
+    // main
+    // antes descuento
+    mostrarComicsEnTabla();
+    mostrarValorAlmacen();
 
-// mostramos el inventario actualizado
-aplicarDescuentoManga();
-mostrarComicsEnTabla();
-mostrarValorAlmacen();
+    // mostramos el inventario actualizado
+    aplicarDescuentoManga();
+    mostrarComicsEnTabla();
+    mostrarValorAlmacen();
 
-function mostrarComicsEnTabla()
-{
-    global $inventario;
-    echo '<br>';
-    echo '<table border="1">';
-    echo '<tr><th>Categoría</th><th>Título</th><th>Editorial</th><th>Autor</th><th>Idioma</th><th>Precio</th><th>Stock</th></tr>';
+    function mostrarComicsEnTabla()
+    {
+        global $inventario;
+        echo '<br>';
+        echo '<table border="1">';
+        echo '<tr><th>Categoría</th><th>Título</th><th>Editorial</th><th>Autor</th><th>Idioma</th><th>Precio</th><th>Stock</th></tr>';
 
-    foreach ($inventario as $categoria => $comics) {
-        foreach ($comics as $comic) {
-            echo '<tr>';
-            echo "<td>$categoria</td>";
-            echo "<td>{$comic['titulo']}</td>";
-            echo "<td>{$comic['editorial']}</td>";
-            echo "<td>{$comic['autor']}</td>";
-            echo "<td>{$comic['idioma']}</td>";
-            echo "<td>{$comic['precio']}</td>";
-            echo "<td>{$comic['stock']}</td>";
-            echo '</tr>';
+        foreach ($inventario as $categoria => $comics) 
+        {
+            foreach ($comics as $comic) 
+            {
+                echo '<tr>';
+                echo "<td>$categoria</td>";
+                echo "<td>{$comic['titulo']}</td>";
+                echo "<td>{$comic['editorial']}</td>";
+                echo "<td>{$comic['autor']}</td>";
+                echo "<td>{$comic['idioma']}</td>";
+                echo "<td>{$comic['precio']}</td>";
+                echo "<td>{$comic['stock']}</td>";
+                echo '</tr>';
+            }
         }
+        echo '</table>';
     }
-    echo '</table>';
-}
 
-function mostrarValorAlmacen()
-{
-    global $inventario;
+    function mostrarValorAlmacen()
+    {
+        global $inventario;
 
-    //Creamos una nueva variable para guardar el valor total
-    $valorTotal = 0;
+        //Creamos una nueva variable para guardar el valor total
+        $valorTotal = 0;
+        
+        //Utilizamos un foreach para recorrer todo el inventario de comics. Multiplicamos el precio por el stock disponible de cada comic y lo vamos sumando a la variable $valorTotal
+        foreach ($inventario as $categoria => $comics) 
+        {
+            foreach ($comics as $comic) 
+            {
+                $precio = $comic['precio'];
+                $stock = $comic['stock'];
+                $valorTotal += ($precio * $stock);
+            }
+        }
+
+        //Mostramos el valor final del almacen
+        echo "Total valor almacen es: " .  $valorTotal;
+        echo "<br>";
+    }
+
+    function aplicarDescuentoManga()
+    {
+        global $inventario; 
     
-    //Utilizamos un foreach para recorrer todo el inventario de comics. Multiplicamos el precio por el stock disponible de cada comic y lo vamos sumando a la variable $valorTotal
-    foreach ($inventario as $categoria => $comics) 
-    {
-        foreach ($comics as $comic) 
+        //Utilizamos un foreach para recorrer todo el inventario de comics. Por cada comic con el idioma "Japonés" le aplicamos el descuento en cuestión.
+        foreach ($inventario as $categoria => &$comics) 
         {
-            $precio = $comic['precio'];
-            $stock = $comic['stock'];
-            $valorTotal += ($precio * $stock);
+            foreach ($comics as &$comic) 
+            {
+                if ($comic['idioma'] == 'Japonés') 
+                {
+                    $comic['precio'] = $comic['precio'] * 0.7; // Aplicamos un descuento del 30%
+                }
+            }
         }
     }
-
-    //Mostramos el valor final del almacen
-    echo "Total valor almacen es: " .  $valorTotal;
-    echo "<br>";
-}
-
-function aplicarDescuentoManga()
-{
-    global $inventario;
-
-    foreach ($inventario['accion'] as &$comic) 
-    {
-        if ($comic['idioma'] == 'Japonés') 
-        {
-            $comic['precio'] = $comic['precio'] * 0.7; // Aplicar descuento del 30%
-        }
-    }
-
-    //Copiamos y pegamos la estructura de arriba, pero cambiando la categoría por "suspense_terror", para calcular su 30%
-    foreach ($inventario['suspense_terror'] as &$comic) 
-    {
-        if ($comic['idioma'] == 'Japonés') 
-        {
-            $comic['precio'] = $comic['precio'] * 0.7; // Aplicar descuento del 30%
-        }
-    }
-
-}
 ?>
 
 
